d3a88135e779b9c89770d7b6f3e7bc2362763783
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2020-02-26 09:22:46 +0300

    Add files via upload

---
+++ 1lab_COM/1lab_COM/1lab_COM.cpp
@@ -0,0 +1,31 @@
+﻿//#include "stdafx.h"
+#include <iostream>
+#include <objbase.h>
+#include "../interface/interface.cpp"
+#include <tchar.h>
+
+//Клиент
+int main()
+{
+	HRESULT hr;
+	trace("Get a pointer to Iunknown");
+	IUnknown* pIUnknown = CreateInstance();
+	trace("Get a pointer to IY");	
+	IY* pIY = NULL;
+	hr = pIUnknown->QueryInterface(IID_IY, (void**)&pIY);
+	if (SUCCEEDED(hr))
+	{
+		trace("IX gotcha successfully!");
+		pIY->Fy();
+	}
+
+	IX* pIX = NULL;
+	hr = pIUnknown->QueryInterface(IID_IX, (void**)&pIX);
+	if (SUCCEEDED(hr))
+	{
+		trace("IX gotcha successfully!");
+		pIX->Fx(9);
+	}
+	//delete pIUnknown;
+	return 0;
+}
